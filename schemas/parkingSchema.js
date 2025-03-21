a8b3d2ef5b322e3c609d340c1a5522746f0e1fcc
Diego_Alvarez <diego.alvarez.vazquez@gmail.com>
2025-03-21 16:38:44 -0600

    Min and Max validation messages modified on Parking creation

--- schemas/parkingSchema.js
+++ schemas/parkingSchema.js
@@ -23,7 +23,10 @@ const VALID_ORDER_TYPES = ['asc', 'desc'];
 
 export const createParkingSchema = Joi.object({
   name: name.required(),
-  spots: spots.required(),
+  spots: spots.required().messages({
+    'number.min': `The parking lot is too small. "spots" must be greater than or equal to ${MIN_SPOTS}`,
+    'number.max': `The parking lot is too big. "spots" must be less than or equal to ${MAX_SPOTS}`,
+  }),
   contact: contact.required().messages({
     'string.pattern.base': '\"contact\" must be a valid phone number',
   }),
