2861481de822e02d60851b4302ed0ec642cc3fe1
Diego_Alvarez <diego.alvarez.vazquez@gmail.com>
2025-03-25 18:12:34 -0600

    Added database for testing

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
 
 export const config = {
   port: process.env.PORT || 3000,
